eca9170b9626dcb3d76c03afb703ae3ae7c87c34
Fernando <ffonseca1986@gmail.com>
2021-02-10 17:10:10 -0300

    versão atual do app

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtonProps extends RectButtonProperties{
+  children:string;
+}
+
+// eslint-disable-next-line react/prop-types
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container {...rest}>
+    <ButtonText>{children}</ButtonText>
+  </Container>
+);
+
+export default Button;
